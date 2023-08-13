fd3699dcfb24fa26d6a18a18dcd9b7122d759069
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2023-08-13 13:35:15 +0300

    сделал домашку

--- src/components/StatisticsList/StatisticsList.jsx
+++ src/components/StatisticsList/StatisticsList.jsx
@@ -4,7 +4,7 @@ export const StatisticsList = ({ data }) => {
   return (
     <ul className={css.stat_list}>
       {data.map(item => (
-        <li key={item.id}>
+        <li key={item.id} className={css.stat_item}>
           <span className={css.label}>{item.label}</span>
           <span className={css.percentage}>{item.percentage}</span>
         </li>
