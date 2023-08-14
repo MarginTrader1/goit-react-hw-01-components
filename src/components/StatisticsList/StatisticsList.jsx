b43d36db29f040d3d6203515b98cbafd8d4c4c54
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2023-08-14 17:17:04 +0300

    сделал рандомный цвет

--- src/components/StatisticsList/StatisticsList.jsx
+++ src/components/StatisticsList/StatisticsList.jsx
@@ -1,10 +1,13 @@
 import css from './StatisticsList.module.css';
+import { color } from 'components/Color';
 
 export const StatisticsList = ({ data }) => {
   return (
     <ul className={css.stat_list}>
       {data.map(item => (
-        <li key={item.id} className={css.stat_item}>
+        <li key={item.id} className={css.stat_item} style={{
+          backgroundColor: color,
+        }}>
           <span className={css.label}>{item.label}</span>
           <span className={css.percentage}>{item.percentage}</span>
         </li>
